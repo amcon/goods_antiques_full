9273737be76ad20b0f6d3ebc74eecccfee566b2e
Aaron Conklin <conklin.aaron@gmail.com>
2017-11-26 16:56:44 -0500

    successful map api

---
+++ src/components/App/Main/Map/MapComponent/MapComponent.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+
+export class MapComponent extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    const {lat, lng} = this.props.initialCenter;
+    this.state = {
+      currentLocation: {
+        lat: lat,
+        lng: lng
+      }
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.google !== this.props.google) {
+      this.loadMap();
+    }
+  }
+
+  componentDidMount() {
+    this.loadMap();
+  }
+
+  loadMap() {
+    if (this.props && this.props.google) {
+      // google is available
+      const {google} = this.props;
+      const maps = google.maps;
+
+      const mapRef = this.refs.map;
+      const node = ReactDOM.findDOMNode(mapRef);
+
+      let {initialCenter, zoom} = this.props;
+      const {lat, lng} = this.state.currentLocation;
+      const center = new maps.LatLng(lat, lng);
+      const mapConfig = Object.assign({}, {
+        center: center,
+        zoom: zoom
+      })
+      this.map = new maps.Map(node, mapConfig);
+    }
+    // ...
+  }
+
+  render() {
+    return (
+      <div ref='map'>
+        Loading map...
+      </div>
+    )
+  }
+}
+
+Map.propTypes = {
+  google: React.PropTypes.object,
+  zoom: React.PropTypes.number,
+  initialCenter: React.PropTypes.object
+}
+
+Map.defaultProps = {
+  zoom: 13,
+  // San Francisco, by default
+  initialCenter: {
+    lat: 37.774929,
+    lng: -122.419416
+  }
+}
+
