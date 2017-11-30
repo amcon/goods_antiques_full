2026eed4ce815ad3343a6097f7896521aea705c3
Aaron Conklin <conklin.aaron@gmail.com>
2017-11-30 15:13:26 -0500

    added webpack for process env

--- webpack.config.js
+++ webpack.config.js
@@ -41,6 +41,11 @@ module.exports = {
     ],
   },
   plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
+      },
+    }),
     new ExtractTextPlugin("styles.css"),
     new GoogleFontsPlugin({
             fonts: [
