bf7b9a197018fecf82d8622ae6c08b677d3e238d
Aaron Conklin <conklin.aaron@gmail.com>
2017-11-30 22:55:08 -0500

    add jsx presets

--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,9 @@ module.exports = {
         use: [
           'babel-loader',
         ],
+        query: {
+          presets:['react','es2015']
+        }
       },
       {
         test: /\.css$/,
