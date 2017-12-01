9c6da72f203d441c7cf051e8ddef421612be5d46
Aaron Conklin <conklin.aaron@gmail.com>
2017-12-01 13:02:56 -0500

    trying to get package.json to run webpack on heroku

--- scripts/deployment_react.sh
+++ scripts/deployment_react.sh
@@ -13,7 +13,7 @@ npm run clean -s
 npm i --only=dev --ignore-scripts
 
 # build out webpack assets
-npm run build:production
+npm run build
 
 # Remove the DEV version of node modules
 rm -rf node_modules
