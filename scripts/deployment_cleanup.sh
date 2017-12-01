9afe9cba1348aac8345a146211407ee6203f081d
Aaron Conklin <conklin.aaron@gmail.com>
2017-11-30 21:56:01 -0500

    dont remove src folder on heroku build

--- scripts/deployment_cleanup.sh
+++ scripts/deployment_cleanup.sh
@@ -2,4 +2,4 @@
 # @author Jason Seminara for General Assembly, 2016
 # @desc Deployment should be as slim as possible, and with any identifying remarks or traces removed (i.e., we don't need to tell anyone how we built our app when it's deployed ;).
 
-rm -rf db src scripts starter_resources lessons .babelrc .gitignore README.md
+rm -rf db scripts starter_resources lessons .babelrc .gitignore README.md
