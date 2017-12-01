14cf851cbfaff892582312345548d6e2e9c6ce48
Aaron Conklin <conklin.aaron@gmail.com>
2017-12-01 16:52:55 -0500

    ajusting scripts to run only npm i --dev

--- scripts/deployment_react.sh
+++ scripts/deployment_react.sh
@@ -10,7 +10,7 @@ npm run clean -s
 
 # Go get all the npm assets we'll need which include all the devDependencies
 # Don't invoke the npm lifecycle methods
-npm i --ignore-scripts
+npm i --only=dev --ignore-scripts
 
 # build out webpack assets
 npm run build
