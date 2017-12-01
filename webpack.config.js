fa425d161e9aa7fb3d7350eacb9aa445da9bb874
Aaron Conklin <conklin.aaron@gmail.com>
2017-11-30 23:06:09 -0500

    add babelrc es2015

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         include: APP_DIR,
-        use: [
+        loader: [
           'babel-loader',
         ],
       },
