13f794fe226042472d8b2925415cc9ba4d00a572
Aaron Conklin <conklin.aaron@gmail.com>
2017-12-02 13:46:46 -0500

    add listen for production

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use(webpackDevMiddleware(compiler, {
 }));
 
 
-const server = app.listen(3000, function() {
+const server = app.listen(process.env.PORT || 3000, function() {
   const host = server.address().address;
   const port = server.address().port;
   console.log('Goods Antiques App listening at http://%s:%s', host, port);
