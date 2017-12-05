9171a06860acbdcf0dc13f8ac2cbcdf85fc2989b
Aaron Conklin <conklin.aaron@gmail.com>
2017-12-05 00:36:59 -0500

    box shadow weirdness

--- src/components/App/Footer/Footer.jsx
+++ src/components/App/Footer/Footer.jsx
@@ -2,32 +2,30 @@ import React from 'react';
 import styles from './Footer.css';
 
 const Footer = () => (
-  <div className="footer-container">
-    <footer className="footer">
-      <div className="footer-section">
-        <h3>STORE HOURS</h3>
-        <p>Mon - Wed: closed</p>
-        <p>Thur - Sat: 10am - 4pm</p>
-        <p>Sunday: 10am - 2pm</p>
-        <p>or by appointment</p>
-      </div>
-      <div className="footer-section">
-        <h3>CONTACT US</h3>
-        <p>(262) 642-2277</p>
-        <a href="mailto:goodsantiqueswisconsin@gmail.com?Subject=After%20Visiting%20Your%20Website..." target="_blank">goodsantiqueswisconsin@gmail.com</a>
-      </div>
-      <div className="footer-section">
-        <h3>LOCATION</h3>
-        <p>Good's Antiques</p>
-        <p>3066 Main Street</p>
-        <p>East Troy, WI 53155</p>
-      </div>
-      <div className="footer-section-social">
-        <a href="https://facebook.com/goodsantiques" target="_blank">facebook</a>
-        <a href="https://instagram.com/goodsantiques" target="_blank">instagram</a>
-      </div>
-    </footer>
-  </div>
+  <footer className="footer">
+    <div className="footer-section">
+      <h3>STORE HOURS</h3>
+      <p>Mon - Wed: closed</p>
+      <p>Thur - Sat: 10am - 4pm</p>
+      <p>Sunday: 10am - 2pm</p>
+      <p>or by appointment</p>
+    </div>
+    <div className="footer-section">
+      <h3>CONTACT US</h3>
+      <p>(262) 642-2277</p>
+      <a href="mailto:goodsantiqueswisconsin@gmail.com?Subject=After%20Visiting%20Your%20Website..." target="_blank">goodsantiqueswisconsin@gmail.com</a>
+    </div>
+    <div className="footer-section">
+      <h3>LOCATION</h3>
+      <p>Good's Antiques</p>
+      <p>3066 Main Street</p>
+      <p>East Troy, WI 53155</p>
+    </div>
+    <div className="footer-section-social">
+      <a href="https://facebook.com/goodsantiques" target="_blank">facebook</a>
+      <a href="https://instagram.com/goodsantiques" target="_blank">instagram</a>
+    </div>
+  </footer>
 )
 
 export default Footer;
--- src/components/App/Header/Header.jsx
+++ src/components/App/Header/Header.jsx
@@ -3,19 +3,17 @@ import { Link } from 'react-router-dom';
 import styles from './Header.css';
 
 const Header = () => (
-  <div className="header-container">
-    <header className="header">
-      <Link to='/'>
-        <div className="logo"></div>
-      </Link>
-      <nav>
-        <Link to='/about'>About</Link>
-        <Link to='/store'>Store</Link>
-        <Link to='/shows'>Shows</Link>
-        <Link to='/map'>Map</Link>
-      </nav>
-    </header>
-  </div>
+  <header className="header">
+    <Link to='/'>
+      <div className="logo"></div>
+    </Link>
+    <nav>
+      <Link to='/about'>About</Link>
+      <Link to='/store'>Store</Link>
+      <Link to='/shows'>Shows</Link>
+      <Link to='/map'>Map</Link>
+    </nav>
+  </header>
 )
 
 export default Header;
