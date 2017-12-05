cfd569865dad6291fca86b4042b3a04814746f35
Aaron Conklin <conklin.aaron@gmail.com>
2017-12-04 22:44:10 -0500

    box shadow not borders

--- src/components/App/Header/Header.jsx
+++ src/components/App/Header/Header.jsx
@@ -3,17 +3,19 @@ import { Link } from 'react-router-dom';
 import styles from './Header.css';
 
 const Header = () => (
-  <header className="header">
-    <Link to='/'>
-      <div className="logo"></div>
-    </Link>
-    <nav>
-      <Link to='/about'>About</Link>
-      <Link to='/store'>Store</Link>
-      <Link to='/shows'>Shows</Link>
-      <Link to='/map'>Map</Link>
-    </nav>
-  </header>
+  <div className="header-container">
+    <header className="header">
+      <Link to='/'>
+        <div className="logo"></div>
+      </Link>
+      <nav>
+        <Link to='/about'>About</Link>
+        <Link to='/store'>Store</Link>
+        <Link to='/shows'>Shows</Link>
+        <Link to='/map'>Map</Link>
+      </nav>
+    </header>
+  </div>
 )
 
 export default Header;
