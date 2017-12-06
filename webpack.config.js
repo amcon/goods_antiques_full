640e0495ee6724ca4bc7890eebfe5cb6c2d8f621
Aaron Conklin <conklin.aaron@gmail.com>
2017-12-06 10:24:25 -0500

    adding back babel-polyfill to entry

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ const BUILD_DIR = path.resolve(__dirname, 'dist');
 const APP_DIR = path.resolve(__dirname, 'src');
 
 const config = {
-  entry: `${__dirname}/src/index.js`,
+  entry: ['babel-polyfill', `${__dirname}/src/index.js`],
   output: {
     path: BUILD_DIR,
     filename: '[name].js',
