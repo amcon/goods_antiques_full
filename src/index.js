c9938e7f8eedfd20b875a2b6488416cbc4a313bd
Aaron Conklin <conklin.aaron@gmail.com>
2017-12-06 10:36:15 -0500

    import babel-polyfill to first import on index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
+import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/App/App.jsx';
-import 'babel-polyfill';
 import './../public/images/favicon.ico';
 
 document.addEventListener('DOMContentLoaded', function() {
