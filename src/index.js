2f27670782787657095ff43da89892532051cc5f
Aaron Conklin <conklin.aaron@gmail.com>
2017-12-06 10:18:44 -0500

    using reactdom.render importing reactdom in index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/App/App.jsx';
 import 'babel-polyfill';
 import './../public/images/favicon.ico';
 
 document.addEventListener('DOMContentLoaded', function() {
-  render((
+  ReactDOM.render((
     <BrowserRouter>
       <App />
     </BrowserRouter>
